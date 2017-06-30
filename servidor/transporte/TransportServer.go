3e5d350aa55d96612a345e45ab344b86729fb5f0
Matheus de Almeida Rosa <matheus@debian>
2017-06-30 11:19:36 -0300

    Melhorando código e logs

--- servidor/transporte/TransportServer.go
+++ servidor/transporte/TransportServer.go
@@ -161,137 +161,77 @@ func main(){
     print := fmt.Println
 
     physical2transport_address := ":8006"
-    transport2physical_address := ":8008"
     transport2app_address := ":8007"
-    buf := make([]byte, 1024)
-
-    var opcao_trasmissao string
-    opcao_trasmissao = "tcp"
-	
+    
     physical2transport_port,err := net.ResolveTCPAddr("tcp",physical2transport_address)
     CheckError(err)
     physical2transport_listener, err := net.ListenTCP("tcp", physical2transport_port)
     CheckError(err)
 
-    if opcao_trasmissao == "udp" {
-    	//lendo dados da camada física
-        physical2transport_connection, err := physical2transport_listener.Accept() 
-        CheckError(err)
-        buffer_size,err := physical2transport_connection.Read(buf)
-        CheckError(err)
-        print(string(buf[0:buffer_size]))
-        print("Mensagem recebida com sucesso da camada física...")
-        physical2transport_connection.Close()
+    rcvpkt := make([]byte, 1024)
+    expectedseqnum := 1
+    app_content := []AppContent{}
+    transport2app_content := ""
 
-        //enviando conteúdo do pacote para a camada de aplicação
-        print("Enviando pacote para a camada de aplicação...")
-        source_address,destination_address := getSourceDestinationPort(string(buf[0:9]))
-        app_content := string(buf[14:])
-        transport2app_port,err := net.ResolveTCPAddr("tcp",destination_address)
-    	CheckError(err)
-        transport2app_connection, err := net.DialTCP("tcp", nil, transport2app_port)
-        CheckError(err)
-    	buffer_size,err = transport2app_connection.Write([]byte(app_content))
-        CheckError(err)
-        transport2app_connection.Close()
-        print("Pacote enviado com sucesso para a aplicação.")
-
-        //recebendo resposta HTTP da camada de aplicação
-        print("Recebendo resposta HTTP da camada de aplicação...")
-        tmp := source_address
-        source_address = destination_address
-        destination_address = tmp
-        app2transport_port,err := net.ResolveTCPAddr("tcp",source_address)
-    	CheckError(err)
-        app2transport_listener, err := net.ListenTCP("tcp", app2transport_port)
-        CheckError(err)
-    	appp2transport_connection, err := app2transport_listener.Accept()
-    	CheckError(err)
-    	buffer_size,err = appp2transport_connection.Read(buf)
-    	appp2transport_connection.Close()
-        
-        //enviando resposta HTTP para a camada física
-        //fazendo cabeçalho UDP
-    	var pdu_content bytes.Buffer
-        application_content := string(buf[0:buffer_size])
-        transport_header := makeTransportHeader(source_address,destination_address,buffer_size)
-        pdu_content.WriteString(transport_header.String())
-        pdu_content.WriteString(application_content)
-        transport2physical_port,err := net.ResolveTCPAddr("tcp",transport2physical_address)
-    	CheckError(err)
-        transport2physical_connection, err := net.DialTCP("tcp", nil, transport2physical_port)
-        CheckError(err)
-    	buffer_size,err = transport2physical_connection.Write([]byte(pdu_content.String()))
-        CheckError(err)
-        transport2physical_connection.Close()
-    } else if opcao_trasmissao == "tcp" {
-
-        rcvpkt := make([]byte, 1024)
-        expectedseqnum := 1
-        app_content := []AppContent{}
-        transport2app_content := ""
+    /*
+        maquina de estados do destinatario
+    */
+    for {
 
         /*
-            maquina de estados do destinatario
+            Recebendo segmento da camada fisica
         */
-        for ; ; {
 
-            /*
-                Recebendo segmento da camada fisica
-            */
-
-            print("Recebendo segmento da camada física...")
-            physical2transport_connection, err := physical2transport_listener.Accept() 
-            CheckError(err)
-            _,err = physical2transport_connection.Read(rcvpkt)
-            rcvpkt_formated := convert(rcvpkt)
-            CheckError(err)
-            _,_,_,data := extract(rcvpkt_formated)
+        print("Recebendo segmento da camada física...")
+        physical2transport_connection, err := physical2transport_listener.Accept() 
+        CheckError(err)
+        _,err = physical2transport_connection.Read(rcvpkt)
+        rcvpkt_formated := convert(rcvpkt)
+        CheckError(err)
+        _,_,_,data := extract(rcvpkt_formated)
+        app_content = append(app_content, AppContent{expectedseqnum, data})
+        /*if (matchSeqNum(string(rcvpkt), expectedseqnum)) {
+            print(string(rcvpkt))
+            print(string(rcvpkt[26:33]))
+            source_port,destination_port,_,data := extract(string(rcvpkt))
             app_content = append(app_content, AppContent{expectedseqnum, data})
-            /*if (matchSeqNum(string(rcvpkt), expectedseqnum)) {
-                print(string(rcvpkt))
-                print(string(rcvpkt[26:33]))
-                source_port,destination_port,_,data := extract(string(rcvpkt))
-                app_content = append(app_content, AppContent{expectedseqnum, data})
-                sndACK := makeACK(strconv.Itoa(expectedseqnum), source_port, destination_port)
-                ACKSend(sndACK, transport2physical_address)
-                expectedseqnum += 1
-            } else {
-                source_port,destination_port,_,_ := extract(string(rcvpkt))
-                sndACK := makeACK(strconv.Itoa(expectedseqnum-1), source_port, destination_port)
-                ACKSend(sndACK, transport2physical_address)
-            }*/
+            sndACK := makeACK(strconv.Itoa(expectedseqnum), source_port, destination_port)
+            ACKSend(sndACK, transport2physical_address)
+            expectedseqnum += 1
+        } else {
+            source_port,destination_port,_,_ := extract(string(rcvpkt))
+            sndACK := makeACK(strconv.Itoa(expectedseqnum-1), source_port, destination_port)
+            ACKSend(sndACK, transport2physical_address)
+        }*/
 
-            if(strings.Contains(rcvpkt_formated,"LASTSEG")){
-                index := strings.Index(rcvpkt_formated,"LASTSEG")
-                print(rcvpkt_formated[index:index+len("LASTSEG")])
-                if (rcvpkt_formated[index:index+len("LASTSEG")] == "LASTSEG") {
-                    physical2transport_connection.Close()
-                    break;
-                }
+        if(strings.Contains(rcvpkt_formated,"LASTSEG")){
+            index := strings.Index(rcvpkt_formated,"LASTSEG")
+            print(rcvpkt_formated[index:index+len("LASTSEG")])
+            if (rcvpkt_formated[index:index+len("LASTSEG")] == "LASTSEG") {
+                physical2transport_connection.Close()
+                break;
             }
         }
-
-        sort.Sort(BySequenceNumber(app_content))
-        for i := 0; i < len(app_content); i++ {
-            transport2app_content += app_content[i].Dado
-        }
-        //deliverData(data,destination_port) <---------- enviar mensagem completa pra camada de aplicacao do servidor
-        print("\n\n\nCONTEUDO\n\n")
-        print(transport2app_content)
-
-
-        //enviando conteúdo para a camada de aplicação
-        print("Enviando conteúdo para a camada de aplicação...")
-        transport2app_port,err := net.ResolveTCPAddr("tcp",transport2app_address)
-        CheckError(err)
-        transport2app_connection, err := net.DialTCP("tcp", nil, transport2app_port)
-        CheckError(err)
-        _,err = transport2app_connection.Write([]byte(transport2app_content))
-        CheckError(err)
-        transport2app_connection.Close()
-        print("Pacote enviado com sucesso para a aplicação.")
-
     }
+
+    sort.Sort(BySequenceNumber(app_content))
+    for i := 0; i < len(app_content); i++ {
+        transport2app_content += app_content[i].Dado
+    }
+    //deliverData(data,destination_port) <---------- enviar mensagem completa pra camada de aplicacao do servidor
+    print("\n\n\nCONTEUDO\n\n")
+    print(transport2app_content)
+
+
+    //enviando conteúdo para a camada de aplicação
+    print("Enviando conteúdo para a camada de aplicação...")
+    transport2app_port,err := net.ResolveTCPAddr("tcp",transport2app_address)
+    CheckError(err)
+    transport2app_connection, err := net.DialTCP("tcp", nil, transport2app_port)
+    CheckError(err)
+    _,err = transport2app_connection.Write([]byte(transport2app_content))
+    CheckError(err)
+    transport2app_connection.Close()
+    print("Pacote enviado com sucesso para a aplicação.")
 
 }
