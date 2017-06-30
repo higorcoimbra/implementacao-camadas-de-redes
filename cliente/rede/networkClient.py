495440131d4de90fad98a01acab2f275a303be98
Higor Coimbra <higor1308@gmail.com>
2017-06-30 13:48:16 -0300

    Configurando prints mais adequados a camada de rede

--- cliente/rede/networkClient.py
+++ cliente/rede/networkClient.py
@@ -36,7 +36,7 @@ def strList2str(transport_data):
 		data += "\n"
 	return data
 
-def sameNetwork(network_ip,client_ip,mask):
+def belongsToNet(network_ip,client_ip,mask):
 
 	ip = []
 	i = 0
@@ -50,6 +50,9 @@ def sameNetwork(network_ip,client_ip,mask):
 		i += 1
 	return True
 
+def sameNetwork(client_ip,destination_ip):
+	return belongsToNet(network_ip,str2intList(client_ip),mask) and belongsToNet(network_ip,str2intList(destination_ip),mask)
+	
 
 HOST = '127.0.0.1'              
 transport2network_port = 8002
@@ -57,20 +60,29 @@ network2physical_port = 8003
 physical2network_port = 8012
 network2transport_port = 8013
 
-#
-# Variáveis para verificar se o IP dado pelo usuário está na suposta mesma rede
-# do servidor
-#
+
 network_ip = [192,168,10,0]
 mask = [255,255,255,0]
 destination_ip = "192.168.10.15"
 client_ip = "192.168.10.10"
+
+print("****** CHECANDO SE OS IPS DE CLIENTE E SERVIDOR ESTÃO NA MESMA REDE ******")
+if(sameNetwork(client_ip,destination_ip)):
+	print("IPs pertencem a mesma rede!")
+	print("\n")
+else:
+	print("IPs não pertencem a mesma rede, pacote será redirecionado a um roteador!")
+	print("\n")
+print("**************************************************************************")
+
 #
 # --------------------- IDA ----------------------------------
 #
 
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("*************** INICIANDO FLUXO DE REQUISIÇÃO AO SERVIDOR ***************")
+print("\n")
 
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (HOST, transport2network_port)
 tcp.bind(orig)
 tcp.listen(1)
@@ -79,11 +91,14 @@ while(1):
 	#
 	# Recebendo dados da camada de transporte
 	#
-	print("Aguardando dados da camada de transporte")
+	print("Aguardando segmento da requisição HTTP da camada de transporte...")
+	print("\n")
 	con, cliente = tcp.accept()
 	transport_data = con.recv(MAX_TRANSPORT_DATASIZE).decode("utf-8")
+	print("***** SEGMENTO RECEBIDO *****")
 	print(transport_data)
-
+	print("*****************************")
+	print("\n")
 
 	#
 	# Montagem do cabeçalho da camada de Redes:
@@ -93,11 +108,16 @@ while(1):
 	network_header = client_ip + "\n" + destination_ip + "\n"
 	network_data = network_header + transport_data
 
+	print("****** DATAGRAMA MONTADO ******")
+	print(network_data)
+	print("*******************************")
+	print("\n")
 
 	#
-	# Enviando mensagem da camada de rede pra camada física
+	# Enviando datagrama pra camada física
 	#
-	print("Enviando mensagem da camada de rede pra camada física...")
+	print("Enviando datagrama da requisição HTTP pra camada física...")
+	print("\n")
 	tcp2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	dest = (HOST, network2physical_port)
 	tcp2.connect(dest)
@@ -112,6 +132,9 @@ con.close()
 # ---------------------- VOLTA ---------------------------------
 #
 
+print("*************** INICIANDO FLUXO DE RESPOSTA DO SERVIDOR ***************")
+print("\n")
+
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (HOST, physical2network_port)
 tcp.bind(orig)
@@ -120,16 +143,16 @@ while(1):
 	#
 	# Receber dados da camada física
 	#
-	print("Aguardando dados da camada física...")
+	print("Aguardando quadro da resposta HTTP da camada física...")
+	print("\n")
 	con, cliente = tcp.accept()
 	msg = con.recv(MAX_TRANSPORT_DATASIZE).decode("utf-8")
-	
 	physical_data = msg
 
-	#
-	# Retirando os ips da camada de rede
-	#
-	ips = physical_data.split("\n")[0:2]
+	print("****** QUADRO RECEBIDO ******")
+	print(physical_data)
+	print("*****************************")
+	print("\n")
 
 	#
 	# Retirando cabeçalho da camada de rede dos dados da camada de transporte
@@ -137,10 +160,16 @@ while(1):
 	transport_data = physical_data.split("\n")[2:]
 	transport_data = strList2str(transport_data)
 
+	print("****** SEGMENTO DESENCAPSULADO ******")
+	print(transport_data)
+	print("*************************************")
+	print("\n")
+
 	#
 	# Enviando dados para camada de transporte
 	#
-	print("Enviando dados para a camada de transporte...")
+	print("Enviando segmento da resposta HTTP para a camada de transporte...")
+	print("\n")
 	tcp2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	dest = (HOST, network2transport_port)
 	tcp2.connect(dest)
--- servidor/rede/networkServer.py
+++ servidor/rede/networkServer.py
@@ -27,6 +27,10 @@ network2physical_port = 8010
 #
 # --------------------- IDA -------------------------------
 #
+
+print("*************** INICIANDO FLUXO DE REQUISIÇÃO AO SERVIDOR ***************")
+print("\n")
+
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (HOST, physical2network_port)
 tcp.bind(orig)
@@ -35,11 +39,16 @@ while(1):
 	#
 	# Receber dados da camada física
 	#
-	print("Aguardando dados da camada física...")
+	print("Aguardando quadro da requisição HTTP da camada física...")
+	print("\n")
 	con, cliente = tcp.accept()
 	msg = con.recv(MAX_TRANSPORT_DATASIZE).decode("utf-8")
-	
 	physical_data = msg
+
+	print("****** QUADRO RECEBIDO ******")
+	print(physical_data)
+	print("*****************************")
+	print("\n")
 
 	#
 	# Retirando os ips da camada de rede
@@ -55,7 +64,8 @@ while(1):
 	#
 	# Enviando dados para camada de transporte
 	#
-	print("Enviando dados para a camada de transporte...")
+	print("Enviando segmento da requisição HTTP para a camada de transporte...")
+	print("\n")
 	tcp2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	dest = (HOST, network2transport_port)
 	tcp2.connect(dest)
@@ -69,10 +79,10 @@ con.close()
 # --------------------- VOLTA ----------------------------
 #
 
-print("\n---Aguardando a volta---\n")
+print("*************** INICIANDO FLUXO DE RESPOSTA DO SERVIDOR ***************")
+print("\n")
 
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 orig = (HOST, transport2network_port)
 tcp.bind(orig)
 tcp.listen(1)
@@ -81,10 +91,14 @@ while(1):
 	#
 	# Recebendo dados da camada de transporte
 	#
-	print("Aguardando dados da camada de transporte")
+	print("Aguardando segmento da resposta HTTP da camada de transporte...")
+	print("\n")
 	con, cliente = tcp.accept()
 	transport_data = con.recv(MAX_TRANSPORT_DATASIZE).decode("utf-8")
+	print("***** SEGMENTO RECEBIDO *****")
 	print(transport_data)
+	print("*****************************")
+	print("\n")
 
 
 	#
@@ -95,10 +109,16 @@ while(1):
 	network_header = ips[1] + "\n" + ips[0] + "\n"
 	network_data = network_header + transport_data
 
+	print("****** DATAGRAMA MONTADO ******")
+	print(network_data)
+	print("*******************************")
+	print("\n")
+
 	#
 	# Enviando mensagem da camada de rede pra camada física
 	#
-	print("Enviando mensagem da camada de rede pra camada física...")
+	print("Enviando datagrama da resposta HTTP pra camada física...")
+	print("\n")
 	tcp2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	dest = (HOST, network2physical_port)
 	tcp2.connect(dest)
